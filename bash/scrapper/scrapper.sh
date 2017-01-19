173f9a1e147766c766f720d783274cc9f3952202
Kacper Polak <kacper@hypequality.com>
2017-01-19 10:57:07 +0100

    refactor(scrapper): Allow to pass url as script argument

--- bash/scrapper/scrapper.sh
+++ bash/scrapper/scrapper.sh
@@ -2,10 +2,32 @@
 # Author: Kacper Polak <kacper@hypequality.com>
 # Git: https://github.com/kacepe/useful-scripts/blob/master/bash/scrapper/scrapper.sh
 
-echo "*********************************************************"
-echo "> URL of page to mirror:"
-read url
-echo "Mirroring... Please wait."
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -q $url
-echo "Done"
-echo "*********************************************************"
+while [[ $# -gt 1 ]]
+do
+key="$1"
+case $key in
+    -u|--url)
+    URL="$2"
+    shift
+    ;;
+    -d|--debug)
+    DEBUG=YES
+    shift
+    ;;
+esac
+shift
+done
+if [[ -z $URL ]] ; then
+    echo "> URL of page to mirror:"
+    read URL
+fi
+echo "> Mirroring: ${URL}"
+echo "> Please wait"
+
+if [[ -z $DEBUG ]] ; then
+    wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -q $URL
+else
+    wget --mirror --convert-links --adjust-extension --page-requisites --no-parent $URL
+fi
+
+echo "> Done"
